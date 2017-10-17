857744066c6d830427fd572faefc2ef1043cfa95
Corey Ford <Corey.Ford@usbank.com>
2017-10-17 08:28:00 -0500

    refactor

--- NodaTimeSpike/LocalDateTimeTests.cs
+++ NodaTimeSpike/LocalDateTimeTests.cs
@@ -1,118 +1,137 @@
 ﻿using System;
 using FluentAssertions;
-using GeoTimeZone;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NodaTime;
-using NodaTime.Text;
-using TimeZoneNames;
 
 namespace NodaTimeSpike
 {
     [TestClass]
-    public class LocalDateTimeTests
+    public class GeoPointHelperTests
     {
+        /// <summary>
+        /// First call is slow, so get that out of the way so timings are more meaningful.
+        /// </summary>
+        /// <param name="testContext"></param>
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            const string dateTime = "2017-07-04 20:30:40";
+            var geoPoint = new GeoPoint(41.866422, -87.616988);
+            var d = DateTime.Parse(dateTime);
+
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().NotBeNullOrEmpty();
+            var date = geoPoint.GetLocalDateTimeOffset(d);
+            date.Should().NotBeSameDateAs(default(DateTimeOffset));
+        }
+
+        [TestMethod]
+        public void ChicagoSummer()
+        {
+            const string dateTime = "2017-07-04 20:30:40";
+            var geoPoint = new GeoPoint(41.866422, -87.616988);
+            var d = DateTime.Parse(dateTime);
+
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("CT");
+
+            var date = geoPoint.GetLocalDateTimeOffset(d);
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-07-04T20:30:40-05:00");
+        }
+
         [TestMethod]
         public void ChicagoChristmas()
         {
             const string dateTime = "2017-12-25 20:30:40";
-            const string timeZone = "America/Chicago";
-
-            var pattern = LocalDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
-            var ldt = pattern.Parse(dateTime).Value;
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
-            date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 12, 25, 20, 30, 40, TimeSpan.FromHours(-6)));
-            Console.WriteLine(date.ToString("s"));
-        }
-
-        [TestMethod]
-        public void ChicagoIndependanceDay()
-        {
-            const string dateTime = "2017-07-04 20:30:40";
-            const string timeZone = "America/Chicago";
+            var geoPoint = new GeoPoint(41.866422, -87.616988);
             var d = DateTime.Parse(dateTime);
 
-            var ldt = new LocalDateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second);
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
-            date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 07, 04, 20, 30, 40, TimeSpan.FromHours(-5)));
-            Console.WriteLine(date.ToString("s"));
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("CT");
+
+            var date = geoPoint.GetLocalDateTimeOffset(d);
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-12-25T20:30:40-06:00");
         }
+
 
         [TestMethod]
         public void KnoxIndependanceDay()
         {
+            const string dateTime = "2017-07-04 20:30:40";
             var geoPoint = new GeoPoint(41.285433, -86.626029);
-            var d = DateTime.Parse("2017-07-04 20:30:40");
+            var d = DateTime.Parse(dateTime);
+
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("CT");
 
             var date = geoPoint.GetLocalDateTimeOffset(d);
-
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 07, 04, 20, 30, 40, TimeSpan.FromHours(-5)));
-            Console.WriteLine(date.ToString("s"));
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-07-04T20:30:40-05:00");
         }
 
         [TestMethod]
         public void KnoxChristmas()
         {
-            const string dateTime = "2017-12-25 20:30:40";
-            var timeZone = TimeZoneLookup.GetTimeZone(41.285433, -86.626029).Result;
-            timeZone.Should().Be("America/Indiana/Knox");
-            var abbreviation = TZNames.GetAbbreviationsForTimeZone(timeZone, "en-US").Generic;
-            abbreviation.Should().Be("CT");
+            var geoPoint = new GeoPoint(41.285433, -86.626029);
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("CT");
 
-            var pattern = LocalDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
-            var ldt = pattern.Parse(dateTime).Value;
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
+            var dateTime = DateTime.Parse("2017-12-25 20:30:40");
+            var date = geoPoint.GetLocalDateTimeOffset(dateTime);
+
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 12, 25, 20, 30, 40, TimeSpan.FromHours(-6)));
-            Console.WriteLine(date.ToString("s"));
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-12-25T20:30:40-06:00");
         }
 
         [TestMethod]
         public void FortWayneIndependanceDay()
         {
-            const string dateTime = "2017-07-04 20:30:40";
-            var timeZone = TimeZoneLookup.GetTimeZone(40.977506, -85.196059).Result;
-            var d = DateTime.Parse(dateTime);
-            var abbreviation = TZNames.GetAbbreviationsForTimeZone(timeZone, "en-US").Generic;
-            abbreviation.Should().Be("ET");
+            var dateTime = DateTime.Parse("2017-07-04 20:30:40");
+            var geoPoint = new GeoPoint(40.977506, -85.196059);
+            var date = geoPoint.GetLocalDateTimeOffset(dateTime);
 
-            var ldt = new LocalDateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second);
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("ET");
+
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 07, 04, 20, 30, 40, TimeSpan.FromHours(-4)));
-            Console.WriteLine(date.ToString("s"));
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-07-04T20:30:40-04:00");
         }
 
         [TestMethod]
         public void FortWayneChristmas()
         {
-            const string dateTime = "2017-12-25 20:30:40";
-            var timeZone = TimeZoneLookup.GetTimeZone(40.977506, -85.196059).Result;
-            timeZone.Should().Be("America/Indiana/Indianapolis");
-            var abbreviation = TZNames.GetAbbreviationsForTimeZone(timeZone, "en-US").Generic;
-            abbreviation.Should().Be("ET");
+            var dateTime = DateTime.Parse("2017-12-25 20:30:40");
+            var geoPoint = new GeoPoint(40.977506, -85.196059);
+            var date = geoPoint.GetLocalDateTimeOffset(dateTime);
 
-            var pattern = LocalDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
-            var ldt = pattern.Parse(dateTime).Value;
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("ET");
+
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 12, 25, 20, 30, 40, TimeSpan.FromHours(-5)));
-            Console.WriteLine(date.ToString("s"));
+            var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
+            Console.WriteLine(s);
+            s.Should().Be("2017-12-25T20:30:40-05:00");
         }
 
         [TestMethod]
         public void TucsonIndependanceDay()
         {
-            const string dateTime = "2017-07-04 20:30:40";
-            var timeZone = TimeZoneLookup.GetTimeZone(32.114510, -110.939259).Result;
-            var d = DateTime.Parse(dateTime);
-            var abbreviation = TZNames.GetAbbreviationsForTimeZone(timeZone, "en-US").Generic;
-            abbreviation.Should().Be("MT");
+            var dateTime = DateTime.Parse("2017-07-04 20:30:40");
+            var geoPoint = new GeoPoint(32.114510, -110.939259);
+            var date = geoPoint.GetLocalDateTimeOffset(dateTime);
 
-            var ldt = new LocalDateTime(d.Year, d.Month, d.Day, d.Hour, d.Minute, d.Second);
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("MT");
+
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 07, 04, 20, 30, 40, TimeSpan.FromHours(-7)));
             var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
             Console.WriteLine(s);
@@ -122,16 +141,13 @@ namespace NodaTimeSpike
         [TestMethod]
         public void TucsonChristmas()
         {
-            const string dateTime = "2017-12-25 20:30:40";
-            var timeZone = TimeZoneLookup.GetTimeZone(32.114510, -110.939259).Result;
-            timeZone.Should().Be("America/Phoenix");
-            var abbreviation = TZNames.GetAbbreviationsForTimeZone(timeZone, "en-US").Generic;
-            abbreviation.Should().Be("MT");
+            var dateTime = DateTime.Parse("2017-12-25 20:30:40");
+            var geoPoint = new GeoPoint(32.114510, -110.939259);
+            var date = geoPoint.GetLocalDateTimeOffset(dateTime);
 
-            var pattern = LocalDateTimePattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
-            var ldt = pattern.Parse(dateTime).Value;
-            var zdt = ldt.InZoneLeniently(DateTimeZoneProviders.Tzdb[timeZone]);
-            var date = zdt.ToDateTimeOffset();
+            var label = geoPoint.LookupTimeZoneLabel();
+            label.Should().Be("MT");
+
             date.ShouldBeEquivalentTo(new DateTimeOffset(2017, 12, 25, 20, 30, 40, TimeSpan.FromHours(-7)));
             var s = date.ToString("yyyy-MM-ddTHH\\:mm\\:sszzz");
             Console.WriteLine(s);
